3570b851b13bc7cea2b28161a2b11fbd09f70469
TheLonestar1 <yura2000077@yandex.ru>
2022-09-14 15:24:41 +0700

    Добавьте файлы проекта.

---
+++ FileManager/Model/IModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManager.Model
+{
+    internal interface IModel
+    {
+        string Path { get; set; }
+        string Name { get; set; }
+        string Size { get; set; }
+        string Icon { get; set; }
+    }
+}
